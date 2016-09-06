fbc36d3d367474fbb180cfa844e9212bce0a27a4
Sean <voidqk@users.noreply.github.com>
2016-09-05 22:43:30 -0400

    lol fixing typo

--- src/sink_shell.js
+++ src/sink_shell.js
@@ -1,4 +1,4 @@
-// (c) Cyright 2016, Sean Connelly (@voidqk), http://syntheti.cc
+// (c) Copyright 2016, Sean Connelly (@voidqk), http://syntheti.cc
 // MIT License
 // Project Home: https://github.com/voidqk/sink
 
