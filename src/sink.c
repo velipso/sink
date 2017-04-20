639c1c29beecfbc031bbd94f32b9a8a79304a1b0
Sean <voidqk@users.noreply.github.com>
2017-04-20 13:07:20 -0400

    little and big endian versions of str.hash

--- src/sink.c
+++ src/sink.c
@@ -14016,10 +14016,96 @@ static inline uint64_t fmix64(uint64_t k){
 	return k;
 }
 
-void sink_str_hashplain(int size, const uint8_t *str, uint32_t seed, uint32_t *out){
+static inline void hash_le(int size, const uint8_t *str, uint32_t seed, uint32_t *out){
 	// MurmurHash3 was written by Austin Appleby, and is placed in the public
 	// domain. The author hereby disclaims copyright to this source code.
 	// https://github.com/aappleby/smhasher
+	uint64_t nblocks = size >> 4;
+	uint64_t h1 = seed;
+	uint64_t h2 = seed;
+	uint64_t c1 = UINT64_C(0x87C37B91114253D5);
+	uint64_t c2 = UINT64_C(0x4CF5AD432745937F);
+
+	const uint64_t *blocks = (const uint64_t *)str;
+
+	for (uint64_t i = 0; i < nblocks; i++){
+		uint64_t k1 = blocks[i * 2 + 0];
+		uint64_t k2 = blocks[i * 2 + 1];
+
+		k1 *= c1;
+		k1 = rotl64(k1, 31);
+		k1 *= c2;
+		h1 ^= k1;
+
+		h1 = rotl64(h1, 27);
+		h1 += h2;
+		h1 = h1 * 5 + 0x52DCE729;
+
+		k2 *= c2;
+		k2 = rotl64(k2, 33);
+		k2 *= c1;
+		h2 ^= k2;
+
+		h2 = rotl64(h2, 31);
+		h2 += h1;
+		h2 = h2 * 5 + 0x38495AB5;
+	}
+
+	const uint8_t *tail = &str[nblocks << 4];
+
+	uint64_t k1 = 0;
+	uint64_t k2 = 0;
+
+	switch(size & 15) {
+		case 15: k2 ^= (uint64_t)(tail[14]) << 48;
+		case 14: k2 ^= (uint64_t)(tail[13]) << 40;
+		case 13: k2 ^= (uint64_t)(tail[12]) << 32;
+		case 12: k2 ^= (uint64_t)(tail[11]) << 24;
+		case 11: k2 ^= (uint64_t)(tail[10]) << 16;
+		case 10: k2 ^= (uint64_t)(tail[ 9]) << 8;
+		case  9: k2 ^= (uint64_t)(tail[ 8]) << 0;
+
+			k2 *= c2;
+			k2 = rotl64(k2, 33);
+			k2 *= c1;
+			h2 ^= k2;
+
+		case  8: k1 ^= (uint64_t)(tail[ 7]) << 56;
+		case  7: k1 ^= (uint64_t)(tail[ 6]) << 48;
+		case  6: k1 ^= (uint64_t)(tail[ 5]) << 40;
+		case  5: k1 ^= (uint64_t)(tail[ 4]) << 32;
+		case  4: k1 ^= (uint64_t)(tail[ 3]) << 24;
+		case  3: k1 ^= (uint64_t)(tail[ 2]) << 16;
+		case  2: k1 ^= (uint64_t)(tail[ 1]) << 8;
+		case  1: k1 ^= (uint64_t)(tail[ 0]) << 0;
+
+			k1 *= c1;
+			k1 = rotl64(k1, 31);
+			k1 *= c2;
+			h1 ^= k1;
+	}
+
+	h1 ^= size;
+	h2 ^= size;
+
+	h1 += h2;
+	h2 += h1;
+
+	h1 = fmix64(h1);
+	h2 = fmix64(h2);
+
+	h1 += h2;
+	h2 += h1;
+
+	out[0] = h1 & 0xFFFFFFFF;
+	out[1] = h1 >> 32;
+	out[2] = h2 & 0xFFFFFFFF;
+	out[3] = h2 >> 32;
+}
+
+static inline void hash_be(int size, const uint8_t *str, uint32_t seed, uint32_t *out){
+	// big-endian version of hash_le... annoyed I can't detect this with a macro at compile-time,
+	// but oh well
 	uint64_t nblocks = size >> 4;
 	uint64_t h1 = seed;
 	uint64_t h2 = seed;
@@ -14117,6 +14203,16 @@ void sink_str_hashplain(int size, const uint8_t *str, uint32_t seed, uint32_t *o
 	out[2] = h2 & 0xFFFFFFFF;
 	out[3] = h2 >> 32;
 }
+
+void sink_str_hashplain(int size, const uint8_t *str, uint32_t seed, uint32_t *out){
+	const uint_least16_t v = 1;
+	const uint8_t *vp = (const uint8_t *)&v;
+	if (*vp)
+		hash_le(size, str, seed, out);
+	else
+		hash_be(size, str, seed, out);
+}
+
 /*
 // utf8
 bool      sink_utf8_valid(sink_ctx ctx, sink_val a);
